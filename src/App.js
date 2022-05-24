dd389a249cd8adc8cade40761dfdd32ee10fbbb0
Suhel Tamboli <suhelmtamboli9@gmail.com>
2022-05-24 06:31:31 +0530

    added shop page, collection-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import { Homepage } from './pages/homepage/homepage.component';
 import './App.css';
+import Header from './components/header/header.component';
+import ShopPage from './pages/shop/shop.component';
+
 
 function App() {
   return (
-    <div >
-      <Homepage />
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
